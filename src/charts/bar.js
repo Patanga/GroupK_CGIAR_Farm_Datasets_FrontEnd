00e57cfcfcce9818e94c330e4f36be777f61685f
hcf720 <98347899+hcf720@users.noreply.github.com>
2022-06-30 22:39:49 +0800

    Workable front-end react app for experimental back-end dashboard api. Only LL page and grouping feature implemented.

---
+++ src/charts/bar.js
@@ -0,0 +1,150 @@
+const echarts = require('echarts');
+
+exports.getBarOption = (barData) => {
+    return {
+        title: {
+            text: 'Total value of activities',
+            subtext: 'Global',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow',
+                label: {
+                    show: true
+                }
+            }
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                mark: { show: true },
+                dataView: { show: true, readOnly: false },
+                // magicType: { show: true, type: ['line', 'bar'] },
+                restore: { show: true },
+                saveAsImage: { show: true }
+            }
+        },
+        calculable: true,
+        legend: {
+            show: false,
+            itemGap: 10,
+        },
+        grid: {
+            top: '12%',
+            left: '1%',
+            right: '10%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                show: false,
+                type: 'category',
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                name: '$ per MAE per day',
+                nameTextStyle: {
+                    align: 'left',
+                },
+                axisLabel: {
+                    formatter: function (a) {
+                        a = +a;
+                        return isFinite(a) ? echarts.format.addCommas(+a / 1000) : '';
+                    }
+                }
+            }
+        ],
+        dataZoom: [
+            {
+                show: true,
+                start: 50,
+                end: 80
+            },
+            {
+                type: 'inside',
+                start: 84,
+                end: 100
+            },
+            {
+                show: true,
+                yAxisIndex: 0,
+                filterMode: 'empty',
+                width: 30,
+                height: '80%',
+                showDataShadow: false,
+                left: '93%'
+            }
+        ],
+        dataset: [{
+            dimensions: [
+                'id_unique',
+                'api_income_lstk_ppp_pd_pmae',
+                'api_income_crop_ppp_pd_pmae',
+                'api_income_offfarm_ppp_pd_pmae',
+                'api_cons_lstk_ppp_pd_pmae',
+                'api_cons_crop_ppp_pd_pmae',
+                'api_income_tot_ppp_pd_pmae',
+                'api_tva'
+            ],
+            source: barData
+        },
+        {
+            transform: {
+                type: 'sort',
+                config: { dimension: 'api_tva', order: 'asc' }
+            }
+        },
+        ],
+        series: [
+            {
+                name: 'Crops Income',
+                type: 'bar',
+                stack: 's',
+                datasetIndex: 1,
+                emphasis: {
+                    focus: 'series'
+                }
+            },
+            {
+                name: 'Livestock Income',
+                type: 'bar',
+                stack: 's',
+                datasetIndex: 1,
+                emphasis: {
+                    focus: 'series'
+                }
+            },
+            {
+                name: 'Off Farm Income',
+                type: 'bar',
+                stack: 's',
+                datasetIndex: 1,
+                emphasis: {
+                    focus: 'series'
+                }
+            },
+            {
+                name: 'Crops Consumed',
+                type: 'bar',
+                stack: 's',
+                datasetIndex: 1,
+                emphasis: {
+                    focus: 'series'
+                }
+            },
+            {
+                name: 'Livestock Consumed',
+                type: 'bar',
+                stack: 's',
+                datasetIndex: 1,
+                emphasis: {
+                    focus: 'series'
+                }
+            },
+        ]
+    }
+}
---
+++ src/data_processors/livelihood.js
@@ -0,0 +1,70 @@
+
+// Total Value of Activities
+// Stacked bar chart
+const appendTVA = (doc) => {
+    doc.api_tva =
+        doc.api_income_lstk_ppp_pd_pmae +
+        doc.api_income_crop_ppp_pd_pmae +
+        doc.api_income_offfarm_ppp_pd_pmae +
+        doc.api_cons_lstk_ppp_pd_pmae +
+        doc.api_cons_crop_ppp_pd_pmae;
+
+    return doc;
+}
+
+exports.getBarData = (data) => {
+    const barData = data.map(doc => appendTVA(doc));
+    return barData;
+}
+
+exports.getPieData = (data) => {
+    let under_1_cnt = 0;
+    let _1to1_9_cnt = 0;
+    let above_1_9_cnt = 0;
+
+    data.map(doc => {
+        if (doc.api_income_tot_ppp_pd_pmae >= 1.9) {
+            above_1_9_cnt++;
+        }
+        else if (doc.api_income_tot_ppp_pd_pmae >= 1) {
+            _1to1_9_cnt++;
+        }
+        else if (doc.api_income_tot_ppp_pd_pmae > 0) {
+            under_1_cnt++;
+        }
+        // Null value not counted
+    })
+    // format data for echarts
+    return [
+        { value: under_1_cnt, name: '< 1 USD' },
+        { value: _1to1_9_cnt, name: '1 to 1.99 USD' },
+        { value: above_1_9_cnt, name: '> 1.99 USD' },
+    ]
+}
+
+exports.getBoxData = (data) => {
+
+    let cons_crop_annual_arr = [];
+    let cons_lstk_annual_arr = [];
+    let incm_crop_annual_arr = [];
+    let incm_lstk_annual_arr = [];
+    let incm_offfarm_annual_arr = [];
+
+    data.map(doc => {
+        const year = doc.year;
+        const days = (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 366 : 365;
+        cons_crop_annual_arr.push(doc.api_cons_crop_ppp_pd_pmae * days);
+        cons_lstk_annual_arr.push(doc.api_cons_lstk_ppp_pd_pmae * days);
+        incm_crop_annual_arr.push(doc.api_income_crop_ppp_pd_pmae * days);
+        incm_lstk_annual_arr.push(doc.api_income_lstk_ppp_pd_pmae * days);
+        incm_offfarm_annual_arr.push(doc.api_income_offfarm_ppp_pd_pmae * days);
+    })
+
+    return [
+        cons_crop_annual_arr,
+        cons_lstk_annual_arr,
+        incm_crop_annual_arr,
+        incm_lstk_annual_arr,
+        incm_offfarm_annual_arr
+    ]
+}
