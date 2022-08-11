f0034a576ac53b557890bca4f73fbb485e5b53a7
hcf720 <98347899+hcf720@users.noreply.github.com>
2022-08-11 10:08:33 +0800

    PlotOptions of Home Page tweaked, added new feature to center the map once grouping option changes

--- src/plotOptions/Home/map.js
+++ src/plotOptions/Home/map.js
@@ -2,22 +2,35 @@ import L from 'leaflet'
 
 var map;
 
+
 export function updateMap(dataset) {
+    var latsum = 0;
+    var lonsum = 0;
+    var valid_cnt = 0;
     // Add a circle for each record from the dataset
     dataset.forEach(e => {
-        if (isNaN(e.gps_lat) || isNaN(e.gps_lat)) {
+        if (isNaN(e.gps_lat) || isNaN(e.gps_lon)) {
             // console.log('NaN in gps [' + e.gps_lat + ', ' + e.gps_lon + '], id_uique: ' + e.id_unique);
             return;
         }
+        valid_cnt++;
+        latsum+=e.gps_lat;
+        lonsum+=e.gps_lon;
         L.circle([e.gps_lat, e.gps_lon], {
             radius: 1000,
             opacity: 0.4,
             color: '#ff4500'
         }).addTo(map).bindPopup('Unique ID: ' + e.id_unique +
-                                '<br/>Country: ' + e.id_country +
-                                '<br/>Region: ' + e.region +
-                                '<br/>Project: ' + e.id_proj);
+            '<br/>Country: ' + e.id_country +
+            '<br/>Region: ' + e.region +
+            '<br/>Project: ' + e.id_proj);
     });
+
+
+
+    map.panTo(new L.LatLng(latsum/valid_cnt, lonsum/valid_cnt));
+
+
 }
 
 export function renderMapTile(dataset) {
